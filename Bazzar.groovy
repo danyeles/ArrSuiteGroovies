70a3dfc147738e84b602bba4e32f053621cd5eeb
danyeles <daniel.espinosa.silva@gmail.com>
2025-03-11 13:54:40 -0600

    Update Bazzar.groovy

--- Bazzar.groovy
+++ Bazzar.groovy
@@ -1,34 +1,16 @@
-pipeline {
-    agent any
-
-    stages {
-        stage('Checkout') {
-            steps {
-                git 'https://github.com/danyeles/BazzarApp'
-            }
-        }
-        stage('Generate Pipeline') {
-            steps {
-                script {
-                    jobDsl scriptText: """
-                    pipelineJob('Deploy BazArr Pipeline') {
-                        definition {
-                            cpsScm {
-                                scm {
-                                    git {
-                                        remote {
-                                            url 'https://github.com/danyeles/BazzarApp'
-                                        }
-                                        branches('master')
-                                        scriptPath('Jenkinsfile')
-                                    }
-                                }
-                            }
-                        }
+pipelineJob('Deploy BazArr Pipeline') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/danyeles/BazzarApp'
                     }
-                    """
+                    branches('master')
+                    scriptPath('Jenkinsfile')
                 }
             }
         }
     }
 }
+
