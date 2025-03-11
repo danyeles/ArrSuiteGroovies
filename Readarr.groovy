84c83eb31723c9e230758696cfc4321f3495de74
danyeles <daniel.espinosa.silva@gmail.com>
2025-03-11 14:09:52 -0600

    Update Readarr.groovy

--- Readarr.groovy
+++ Readarr.groovy
@@ -1,32 +1,13 @@
-pipeline {
-    agent any
-
-    stages {
-        stage('Checkout') {
-            steps {
-                git 'https://github.com/danyeles/ReadarrApp.git'
-            }
-        }
-        stage('Generate Pipeline') {
-            steps {
-                script {
-                    jobDsl scriptText: """
-                    pipelineJob('Deploy ReadArr Pipeline') {
-                        definition {
-                            cpsScm {
-                                scm {
-                                    git {
-                                        remote {
-                                            url 'https://github.com/danyeles/ReadarrApp.git'
-                                        }
-                                        branches('master')
-                                        scriptPath('Jenkinsfile')
-                                    }
-                                }
-                            }
-                        }
+pipelineJob('Deploy ReadArr Pipeline') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/danyeles/ReadarrApp.git'
                     }
-                    """
+                    branches('master')
+                    scriptPath('Jenkinsfile')
                 }
             }
         }
